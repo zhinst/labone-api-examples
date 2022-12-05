f5137041c0261b2df446c7eb2cfd6e10c6a8115e
Elena Acinapura <elena.acinapura@zhinst.com>
2022-12-05 09:37:21 +0000

    Refactor HDAWG python and matlab examples

--- hdawg/matlab/hdawg_example_awg_grouped_mode.m
+++ hdawg/matlab/hdawg_example_awg_grouped_mode.m
@@ -1,6 +1,5 @@
-function hdawg_example_awg_commandtable(device_id)
-% Demonstrate how to connect to a Zurich Instruments HDAWG and upload and run an AWG program using 
-%the command table.
+function hdawg_example_awg_grouped_mode(device_id)
+% Demonstrate how to connect to a Zurich Instruments HDAWG and run it in grouped mode.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % USAGE 
@@ -35,6 +34,7 @@ clear ziDAQ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Check correct configuration
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO: Switch to new way of connecting to the device
 % Check that device_id has been correctly provided
 if ~exist('device_id', 'var')
     error(['No value for device_id specified. The first argument to the ' ...
@@ -80,61 +80,70 @@ while(true)
     end
 
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Basic configuration
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Specify the device configuration. Firstly select the AWG core, and therefore the output channels.
-% Then set the output range for each channels and output gains. Finally, activate the outputs.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-awg_c = 0;      % AWG core
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% In this example the device is configured to control groups of 4 outputs with a single sequencer
+% program (channel grouping 1). It is also possible to control groups of 2 outputs (channel grouping
+% 0) or 8 outputs (channel grouping 2).
+% After specifying the grouping mode, specify which group to use and set the output gains for each
+% AWG core in the group. Then set the output range for each channel in the group and switch the
+% channels on.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+grouping = 1;   % Channel grouping 2x4
+awg_group = 0;  % AWG group
 range = 1.0;    % Output range [V]
-ch1 = 2*awg_c;  % Output channels
-ch2 = 2*awg_c+1;
 
-awg_c_s = num2str(awg_c);   % Strings
-ch1_s = num2str(ch1);
-ch2_s = num2str(ch2);
+awg_cores = awg_group * 2^grouping + (0:2^grouping-1);
+channels = awg_group * 2^(grouping+1) + (0:2^grouping-1);
 
-% Configure the HDAWG to use one sequencer for each pair of output channels
-ziDAQ('setInt', ['/', device_id '/system/awg/channelgrouping'], 0);
-% Set the output gains matrix to identity
-ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/0/gains/0'], 1.0);
-ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/0/gains/1'], 0.0);
-ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/1/gains/0'], 0.0);
-ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/1/gains/1'], 1.0);
-% Turn off modulation mode
-ziDAQ('setInt', ['/' device_id '/awgs/' awg_c_s '/outputs/0/modulation/mode'], 0);
-ziDAQ('setInt', ['/' device_id '/awgs/' awg_c_s '/outputs/1/modulation/mode'], 0);
-% Set the output range
-ziDAQ('setDouble', ['/' device_id '/sigouts/' ch1_s '/range'], range);
-ziDAQ('setDouble', ['/' device_id '/sigouts/' ch2_s '/range'], range);
-% Turn on signal output 1 and signal output 2
-ziDAQ('setInt', ['/' device_id '/sigouts/' ch1_s '/on'], 1);
-ziDAQ('setInt', ['/' device_id '/sigouts/' ch2_s '/on'], 1);
+% Grouping mode
+ziDAQ('setInt', ['/', device_id '/system/awg/channelgrouping'], grouping);
+
+% Per-core settings
+for awg_c = awg_cores
+    awg_c_s = num2str(awg_c);
+    ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/0/gains/0'], 1.0);    % Set the output gains matrix to identity
+    ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/0/gains/1'], 0.0);
+    ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/1/gains/0'], 0.0);
+    ziDAQ('setDouble', ['/' device_id '/awgs/' awg_c_s '/outputs/1/gains/1'], 1.0);
+    ziDAQ('setInt', ['/' device_id '/awgs/' awg_c_s '/outputs/0/modulation/mode'], 0); % Turn off modulation mode
+    ziDAQ('setInt', ['/' device_id '/awgs/' awg_c_s '/outputs/1/modulation/mode'], 0);
+end
+
+% Per-channel settings
+for channel = channels
+    ch_s = num2str(channel);
+    ziDAQ('setDouble', ['/' device_id '/sigouts/' ch_s '/range'], range);    % Set the output range
+    ziDAQ('setInt', ['/' device_id '/sigouts/' ch_s '/on'], 1);              % Turn on signal output
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% AWG sequencer program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Define an AWG program as a string stored in the variable `awg_program`, equivalent to what would be
 % entered in the Sequence Editor window in the graphical UI. Differently to a self-contained program,
-% this example refers to a command table by the instruction `executeTableEntry`, and to a placeholder
-% waveforms `p1` and `p2` by the instruction `placeholder`. Both the command table and the waveform
-% data for the placeholders need to be uploaded separately before this sequence program can be run.
+% this example refers to a command table by the instruction `executeTableEntry`, and to placeholder
+% waveforms `p1`, `p2`, `p3`, `p4` by the instruction `placeholder`. Both the command table and the
+% waveform data for the placeholders need to be uploaded separately before this sequence program can
+% be run.
 
-% After defining the sequencer program, this must be compiled before being uploaded. Here the
-% compilation is performed using the LabOne module awgModule.
+% After defining the sequencer program, this must be compiled before being uploaded. The compilation
+% is done using the LabOne module `awgModule`.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Define the AWG program
-wfm_index = 10; % Index of the slot in the waveform memory
+% AWG SeqC program
+wfm_index = 10;
 wfm_length = 1024;
 awg_program_lines = {...
      '// Define placeholder with 1024 samples',...
      sprintf('wave p1 = placeholder(%d);', wfm_length),...
      sprintf('wave p2 = placeholder(%d);', wfm_length),...
+     sprintf('wave p3 = placeholder(%d);', wfm_length),...
+     sprintf('wave p4 = placeholder(%d);', wfm_length),...
      '',...
      '// Assign an index to the placeholder waveform',...
-     sprintf('assignWaveIndex(1,p1, 2,p2, %d);', wfm_index),...
+     sprintf('assignWaveIndex(1,p1, 2,p2, %d);',wfm_index),...
+     sprintf('assignWaveIndex(3,p3, 4,p4, %d);',wfm_index),...
      '',...
      'while(true) {',...
      '   executeTableEntry(0);',...
@@ -144,7 +153,7 @@ awg_program = sprintf('%s\n',awg_program_lines{:});
 % Create an instance of the AWG Module
 awgModule = ziDAQ('awgModule');
 ziDAQ('set', awgModule, 'device', device_id);
-ziDAQ('set', awgModule, 'index', awg_c);
+ziDAQ('set', awgModule, 'index', awg_group);
 ziDAQ('execute', awgModule);
 disp('Starting compilation of the AWG sequence.');
 
@@ -181,13 +190,16 @@ end
 
 % Wait for sequence upload to finish
 timeout = 10;
-time_start = tic;
-while ziDAQ('getInt', ['/' device_id '/awgs/' awg_c_s '/ready']) == 0
-    if toc(time_start) > timeout
-        ziDAQ('clear', awgModule);
-        error('Failed to upload sequence after %.1f s.', timeout);
+start_time = tic;
+for awg_core = awg_cores
+    awg_c_s = num2str(awg_core);
+    while ziDAQ('getInt', ['/' device_id '/awgs/' awg_c_s '/ready']) == 0
+        if toc(start_time) > timeout
+            ziDAQ('clear', awgModule);
+            error('Failed to upload sequence after %.1f s.', timeout);
+        end
+        pause(0.01);
     end
-    pause(0.01);
 end
 disp('Sequence successfully uploaded.')
 
@@ -200,33 +212,49 @@ ziDAQ('clear', awgModule);
 % The waveforms are played by a command table, whose structure must conform to a defined schema. The
 % schema can be read at https://docs.zhinst.com/hdawg/commandtable/v1_1/schema
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Create the command table
-ct = struct();
-ct.header.version = '1.1.0';    % version specification is mandatory
-ct.table{1}.index = 0;
-ct.table{1}.waveform.index = wfm_index;
-ct.table{1}.amplitude0.value = 1.0;
-ct.table{1}.amplitude1.value = 1.0;
-ct_json_str = jsonencode(ct); % Encode the command table into a JSON string
+% Create the command tables
+% First AWG core
+ct_list_1 = struct();
+ct_list_1.header.version = '1.1.0';    %version specification is mandatory
+ct_list_1.table{1}.index = 0;
+ct_list_1.table{1}.waveform.index = wfm_index;
+ct_list_1.table{1}.amplitude0.value = 1.0;
+ct_list_1.table{1}.amplitude1.value = 1.0;
+% Encode the command table into a JSON string
+ct_json_str_list_1 = jsonencode(ct_list_1);
+
+% Second AWG core
+ct_list_2 = struct();
+ct_list_2.header.version = '1.1.0';    %version specification is mandatory
+ct_list_2.table{1}.index = 0;
+ct_list_2.table{1}.waveform.index = wfm_index;
+ct_list_2.table{1}.amplitude0.value = 1.0;
+ct_list_2.table{1}.amplitude1.value = 1.0;
+% Encode the command table into a JSON string
+ct_json_str_list_2 = jsonencode(ct_list_2);
 
 % Upload the command table and wait until the upload has finished
 disp('Uploading the command table.');
-ziDAQ('setVector', ['/' device_id '/awgs/' awg_c_s '/commandtable/data'], ct_json_str);
+ziDAQ('setVector', ['/' device_id '/awgs/' num2str(awg_cores(1)) '/commandtable/data'], ct_json_str_list_1);
+ziDAQ('setVector', ['/' device_id '/awgs/' num2str(awg_cores(2)) '/commandtable/data'], ct_json_str_list_2);
 
 timeout = 10;
 start_time = tic;
-while(true)
-    ct_status = ziDAQ('getInt', ['/' device_id '/awgs/' awg_c_s '/commandtable/status']);
-    if ct_status == 1
-        % Upload successful
-        break
-    elseif ct_status == 8
-        % Error in command table
-        error('The upload of the command table failed.')
-    end
-    if toc(start_time) > timeout
-        % Timeout
-        error('Command table not uploaded within %.1f s.', timeout)
+for awg_core = awg_cores
+    awg_c_s = num2str(awg_core);
+    while(true)
+        ct_status = ziDAQ('getInt', ['/' device_id '/awgs/' awg_c_s '/commandtable/status']);
+        if ct_status == 1
+            % Upload successful
+            break
+        elseif status == 8
+            % Error in command table
+            error('The upload of the command table failed.')
+        end
+        if toc(start_time) > timeout
+            % Timeout
+            error('Command table not uploaded within %.1f s.', timeout)
+        end
     end
 end
 
@@ -238,7 +266,6 @@ end
 % must be the same specified by the `assignWaveIndex` sequencer instruction.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Define the waveform
 x = linspace(-round(wfm_length/2), round(wfm_length/2), wfm_length);
 sigma = round(wfm_length/8);
 waveform1 = exp(x.^2 ./ (2*sigma^2));
@@ -250,14 +277,17 @@ waveform2_int16 = typecast(int16((pow2(15) - 1)*waveform2), 'int16');
 waveform_native = reshape([waveform1_int16; waveform2_int16], 1,[]);
 
 % Upload the waveform to the device
-ziDAQ('setVector',['/' device_id '/awgs/' awg_c_s '/waveform/waves/' num2str(wfm_index)], waveform_native);
+for awg_core = awg_cores
+    awg_c_s = num2str(awg_core);
+    ziDAQ('setVector',['/' device_id '/awgs/' awg_c_s '/waveform/waves/' num2str(wfm_index)], waveform_native);
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Enable the AWG
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('Enabling the AWG.');
-ziDAQ('setInt', ['/' device_id '/awgs/' awg_c_s '/single'], 1);
-ziDAQ('syncSetInt', ['/' device_id '/awgs/' awg_c_s '/enable'], 1);
+ziDAQ('setInt', ['/' device_id '/awgs/' num2str(awg_cores(1)) '/single'], 1);
+ziDAQ('syncSetInt', ['/' device_id '/awgs/' num2str(awg_cores(1)) '/enable'], 1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('Example ended successfully!')
 end
